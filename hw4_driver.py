6787cc16ae133905a828ec9a5a99fce71cc6fa15
Alexandra Nava <aenava@ucsc.edu>
2024-03-01 21:36:54 -0800

    cleaning cholesky

--- hw4_driver.py
+++ hw4_driver.py
@@ -8,14 +8,32 @@ cholesky_decomp = LinAl.Cholesky_Decomposition
 get_data = LinAl.get_matrix = LinAl.Substitution
 chol_sub = LinAl.Substitution
 
+def cholesky_solve(A,B):
+  A = np.array(A)
+  A_original = np.copy(A) #wtf 
+  B = np.array(B)
+  LU = cholesky_decomp(A, " ", " ")
+  #going to try numpy again 
+  LU = np.array(LU)
+  #iterating through B columns in AX=B
+  X = []
+  for column in range(B.shape[1]):
+    print("Working on column " + str(column) + " of B...")
+    b = B[:, column] 
+    x = chol_sub(LU, b)
+    X.append(x)
+  X = np.array(X)
+  X = np.transpose(X)
+  return A_original, X
+
 if __name__== "__main__":
   data = "atkinson.dat"
   A = [[2.0, -1.0, 0.0], 
        [-1.0, 2.0, -1.0],
        [0.0, -1.0, 2.0]]
-  B = [[2,0],
-       [-4,0],
-       [3,0]]
+  B = [[2,1],
+       [-4,-2],
+       [3,5]]
   show_plot = 0
   perform_cholesky = 1
 
@@ -27,20 +45,6 @@ if __name__== "__main__":
 
   'cholesky----------'
   if perform_cholesky == 1:
-    A = np.array(A)
-    A_original = np.copy(A) #wtf 
-    B = np.array(B)
-    LU = cholesky_decomp(A, " ", " ")
-    #going to try numpy again 
-    LU = np.array(LU)
-    #iterating through B columns in AX=B
-    X = []
-    for column in range(B.shape[1]):
-      print("Working on column " + str(column) + " of B...")
-      b = B[:, column] 
-      x = chol_sub(LU, b)
-      X.append(x)
-    X = np.array(X)
-    X = np.transpose(X)
-  #print(A_original@X) #returns B, -> sub works
+    A_original, X = cholesky_solve(A,B)
+  print(A_original @ X) #returns B, -> sub works
 
