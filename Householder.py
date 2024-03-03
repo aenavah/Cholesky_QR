59b17fc99aecb75880d86589d3ed16aa6dadf19f
Alexandra Nava <aenava@ucsc.edu>
2024-03-03 02:59:35 -0800

    working QR

--- Householder.py
+++ Householder.py
@@ -1,39 +1,42 @@
 import numpy as np 
 import scipy
 
+import numpy as np
+import scipy.linalg
+
 def householder(A):
-  m,n = np.shape(A)
-  
-  for j in range(0, n):
-    vj = np.zeros(m)
+    m, n = np.shape(A)
     
-    sign_sj = np.sign(A[j,j])
-    norm = np.sqrt(sum(A[j:,j]**2)) #from j on 
-    s_j = sign_sj * norm 
+    Q = np.eye(m)
+    for j in range(0, n):
+      vj = np.zeros(m)
+      
+      sign_sj = np.sign(A[j, j])
+      norm = np.sqrt(sum(A[j:, j]**2))  #[j,m)
+      s_j = sign_sj * norm
 
-    vj[j] = A[j, j] + s_j
+      vj[j] = A[j, j] + s_j
 
-    for i in range(j + 1, m):
-      vj[i] = A[i,j]
+      for i in range(j + 1, m):
+        vj[i] = A[i, j]
 
-    vj = np.transpose(vj)
-    vj_norm = np.sqrt(sum(vj[:])**2)
-    
-    print("vj_norm", vj_norm)
-    vj = vj/vj_norm
-    vj_T = np.transpose(vj)
+      vj = vj.reshape((-1, 1))
+      vj_norm = np.sqrt((np.sum(vj**2)))
+      
+      vj = vj / vj_norm
+      vj_T = np.transpose(vj)
 
-
-    A = A - 2*(vj @ vj_T) * A
-  return A
-
+      A = A - 2 * (vj @ vj_T @ A)
+      Q = Q @ (np.eye(m) - 2 *(vj @ vj_T))
+    R = A
+    return Q, R
 
 test = np.array([
-  [1, 2],
-  [3, 4],
-  [5, 6]])
+    [1, 2],
+    [3, 4],
+    [5, 6]])
 
-H = householder(test)
-print(H)
-Q, R = scipy.linalg.qr(test)
-print(R)
+Q_mine, R_mine = householder(test)
+#Q, R = scipy.linalg.qr(test)
+#print(Q_mine @ R_mine) # = A -> right 
+
